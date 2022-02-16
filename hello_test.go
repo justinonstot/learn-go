51275056336b099c74aea9b36d35796306b17897
Justin Onstot <justin.onstot@koddi.com>
2022-02-16 09:49:22 -0800

    removed subfolder

--- hello_test.go
+++ hello_test.go
@@ -2,9 +2,9 @@ package hello
 
 import "testing"
 
-func TestHello(t *testing.T)  {
-	want :=  "Hello, world."
+func TestHello(t *testing.T) {
+	want := "Hello, world."
 	if got := Hello(); got != want {
-		t.Errorf("Hello() = %q, want %q", go, want)
+		t.Errorf("Hello() = %q, want %q", got, want)
 	}
 }
